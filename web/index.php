0b41e935b54b4f9aa11807b8081956b9442e2307
astrike <astrike_pro@users.noreply.github.com>
2018-03-13 21:02:40 +0300

    two

--- web/index.php
+++ web/index.php
@@ -2,23 +2,49 @@
 
 class worked
 {
-public $name;
-public $age ;
-public $salary;
+private $name;
+private $age ;
+private $salary;
+
+        private function checkAge($pr)
+        {if ($pr < 100 and $pr > 1)
+        {return true;}}
+
+        public function setName($Pname)
+        {    $this->name = $Pname;}
+
+        public function getName()
+        {    return $this->name;}
+
+        public function setAge($Page)
+        {   if ($this->checkAge($Page)) {
+            $this->age = $Page;}
+            else echo 'Некоректный возраст';}
+
+        public function getAge()
+        {       return $this->age;}
+
+        public function setSalary($Psalary)
+        {    $this->salary = $Psalary;}
+
+        public function getSalary()
+        {    return $this->salary;}
+
+
 }
 
 $rab1 = new worked();
-$rab1->name = 'Иван';
-$rab1->age = 25;
-$rab1->salary = 1000;
+$rab1->setName('Иван');
+$rab1->setAge(25);
+$rab1->setSalary(1000);
 
 $rab2 = new worked();
-$rab2->name = 'Вася';
-$rab2->age = 26;
-$rab2->salary = 2000;
+$rab2->setName('Вася');
+$rab2->setAge(26);
+$rab2->setSalary(2000);
 
-$sumSalary = $rab1->salary + $rab2->salary;
-$sumAge = $rab1->age + $rab2->age;
+$sumSalary = $rab1->getSalary() + $rab2->getSalary();
+$sumAge = $rab1->getAge() + $rab2->getAge();
 
 
 echo 'Сумма зарплат Вани и Васи = '.$sumSalary.'<br/>';
