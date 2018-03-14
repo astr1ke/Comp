56c2003816e2b0d3d3ac05deafbab86be9a65ee2
astrike <astrike_pro@users.noreply.github.com>
2018-03-14 09:21:45 +0300

    two

--- web/index.php
+++ web/index.php
@@ -1,53 +1,77 @@
 <?php
 
-class worked
+
+class user
 {
-private $name;
-private $age ;
-private $salary;
 
-        private function checkAge($pr)
-        {if ($pr < 100 and $pr > 1)
-        {return true;}}
+    protected $name;
+    protected $age;
 
-        public function setName($Pname)
-        {    $this->name = $Pname;}
-
-        public function getName()
-        {    return $this->name;}
-
-        public function setAge($Page)
-        {   if ($this->checkAge($Page)) {
-            $this->age = $Page;}
-            else echo 'Некоректный возраст';}
-
-        public function getAge()
-        {       return $this->age;}
-
-        public function setSalary($Psalary)
-        {    $this->salary = $Psalary;}
-
-        public function getSalary()
-        {    return $this->salary;}
-
+    public function setName($Sname)
+    {$this->name = $Sname;}
+    public function getName()
+    {return $this->name;}
+    public function setAge($Sage)
+    {{$this->age = $Sage;}}
+    public function getAge()
+    {return $this->age;}
 
 }
 
-$rab1 = new worked();
-$rab1->setName('Иван');
-$rab1->setAge(25);
-$rab1->setSalary(1000);
+class worker extends user
+{
+    private $salary;
 
-$rab2 = new worked();
-$rab2->setName('Вася');
-$rab2->setAge(26);
-$rab2->setSalary(2000);
+    public function getSalary()
+    {return $this->salary;}
+    public function setSalary($Ssalary)
+    {$this->salary = $Ssalary;}
 
-$sumSalary = $rab1->getSalary() + $rab2->getSalary();
-$sumAge = $rab1->getAge() + $rab2->getAge();
+}
+
+class Student extends user
+{
+
+    private $stipend;
+    private $kurs;
+
+    public function setStipend($Sstipend)
+    {$this->stipend = $Sstipend;}
+    public function getStipend()
+    {return $this->stipend;}
+    public function setKurs($Skurs)
+    {{$this->kurs = $Skurs;}}
+    public function getKurs()
+    {return $this->kurs;}
+
+}
+
+class Driver extends worker
+{
+
+    private $VStage;
+    private $VKat;
+
+    public function setVStage($SVStage)
+    {$this->VStage = $SVStage;}
+    public function getVStage()
+    {return $this->VStage;}
+    public function setVKat($SVKat)
+    {{$this->VKat = $SVKat;}}
+    public function getVKat()
+    {return $this->VKat;}
+}
 
 
-echo 'Сумма зарплат Вани и Васи = '.$sumSalary.'<br/>';
-echo 'Сумма возрастов Вани и Васи = '.$sumAge;
+$work1 = new worker();
+$work1->setAge(25);
+$work1->setName('Иван');
+$work1->setSalary(1000);
 
+$work2 = new worker();
+$work2->setAge(26);
+$work2->setName('Вася');
+$work2->setSalary(2000);
 
+$res = $work1->getSalary() + $work2->getSalary();
+echo 'Сумма зарплат Васи и Ивана равна '.$res;
