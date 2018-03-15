cbabd9444b6578ab59b0a94471c84df677c3d4b7
astrike <astrike_pro@users.noreply.github.com>
2018-03-15 22:43:37 +0300

    four

--- web/index.php
+++ web/index.php
@@ -72,28 +72,103 @@ class Form
     }
 }
 
+class SmartForm
+{
+    public $input;
+    public $password;
+    public $textarea;
+
+    public function __construct(){
+        $this->input = $_POST['name'];
+        $this->password = $_POST['pass'];
+        $this->textarea = $_POST['123'];
+    }
+
+
+
+}
+
+class Cookie
+{
+
+    public function set($Cname,$Cval){
+        setcookie($Cname,$Cval);
+    }
+
+    public function get($Cname){
+        return $_COOKIE[$Cname];
+    }
+
+    public function del($Cname){
+        unset($_COOKIE[$Cname]);
+    }
+
+}
+
+class Session
+{
+    public function __construct()
+    {
+        session_start();
+    }
+
+    static function set($Sname,$Sval){
+        $_SESSION[$Sname] = $Sval;
+    }
+
+    static function get($Sname){
+        return $_SESSION[$Sname];
+    }
+
+    static function del($Sname){
+        unset($_SESSION[$Sname]);
+    }
+
+    static function prov($Sname){
+        if (isset($_SESSION[$Sname])){
+            return true;
+        } else return false;
+    }
+
+}
+
+class Flash
+{
+    public function setMessage($Cname,$Cval)
+    {
+        Session::set($Cname,$Cval);
+    }
+    public function getMessage($Cname)
+    {
+       return Session::get($Cname);
+    }
+
+}
+
+class Db
+{
+
+    public function getD(){}
+
+    public  function delD(){}
+
+    public function editD(){}
+
+    public function conD(){}
+
+    public function clrT(){}
+
+    public function clrTs(){}
+
+}
 
 $form = new Form();
 
 echo $form->open(['action'=>'index.php', 'method'=>'POST']);
-echo "<br/>";
-
-echo $form->input(['type'=>'text', 'value'=>'!!!']);
-//Код выше выведет <input type="text" value="!!!">
-echo "<br/>";
-
-echo $form->password(['value'=>'!!!']);
-//Код выше выведет <input type="password" value="!!!">
-echo "<br/>";
-
-echo $form->textarea(['placeholder'=>'123', 'value'=>'!!!']);
-//Код выше выведет <textarea placeholder="123">!!!</textarea>
-echo "<br/>";
-
-echo $form->submit(['value'=>'go']);
-//Код выше выведет <input type="submit" value="go">
-echo "<br/>";
-
+echo $form->input(['type'=>'text', 'placeholder'=>'Ваше имя', 'name'=>'name']);
+echo $form->password(['placeholder'=>'Ваш пароль', 'name'=>'pass']);
+echo $form->submit(['value'=>'Отправить']);
 echo $form->close();
-//Код выше выведет </form>
+$form2 = new SmartForm();
+
 
